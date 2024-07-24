dfabba33a3338f37400baf905d211ded1c9d05b7
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-07-24 15:06:09 +0330

    convert perfect mony to service

--- app/Http/Controllers/Panel/PanelController.php
+++ app/Http/Controllers/Panel/PanelController.php
@@ -10,6 +10,7 @@ use App\Models\Bank;
 use App\Models\Doller;
 use App\Models\FinanceTransaction;
 use App\Models\Invoice;
+use App\Models\Payment;
 use App\Models\Service;
 use App\Models\Voucher;
 use App\Notifications\IsEmptyUserInformationNotifaction;
@@ -37,137 +38,15 @@ class PanelController extends Controller
 
     public function purchase()
     {
-        $banks=Bank::where('is_active',1)->get();
+        $banks = Bank::where('is_active', 1)->get();
         $services = Service::all();
         $dollar = Doller::orderBy('id', 'desc')->first();
-        return view('Panel.Purchase.index', compact('services', 'dollar','banks'));
+        return view('Panel.Purchase.index', compact('services', 'dollar', 'banks'));
     }
 
     public function store(PurchaseRequest $request)
     {
-        try {
-            $inputs = $request->all();
-            $dollar = Doller::orderBy('id', 'desc')->first();
-            $balance = Auth::user()->getCreaditBalance();
-            $user = Auth::user();
-            $inputs['user_id'] = $user->id;
-
-            if (isset($inputs['service_id'])) {
-                $service = Service::find($inputs['service_id']);
-
-                $voucherPrice = $dollar->amount_to_rials * $service->amount;
-
-                if ($voucherPrice > $balance) {
-                    return redirect()->route('panel.purchase.view')->withErrors(['Low_inventory' => "موجودی کیف پول شما کافی نیست"]);
-                }
-                $inputs['final_amount'] = $voucherPrice;
-                $inputs['type'] = 'service';
-                $invoice = Invoice::create($inputs);
-
-                $PM = new PerfectMoneyAPI(env('PM_ACCOUNT_ID'), env('PM_PASS'));
-
-                $PMeVoucher = $PM->createEV(env('PAYER_ACCOUNT'), $service->amount);
-                $voucher = Voucher::create(
-                    [
-                        'user_id' => $user->id,
-                        'service_id' => $inputs['service_id'],
-                        'invoice_id' => $invoice->id,
-                        'status' => 'requested',
-                        'description' => 'ارسال در خواست به سروریس پرفکت مانی'
-                    ]
-                );
-                if (is_array($PMeVoucher) and isset($PMeVoucher['VOUCHER_NUM']) and isset($PMeVoucher['VOUCHER_CODE'])) {
-                    $voucher->update([
-                        'status' => 'finished',
-                        'description' => 'ارتباط با سروریس پرفکت مانی موفقیت آمیز بود',
-                        "serial" => $PMeVoucher['VOUCHER_NUM'],
-                        'code' => $PMeVoucher['VOUCHER_CODE']
-                    ]);
-                    Log::emergency("panel Controller :" . json_encode($PMeVoucher));
-                    FinanceTransaction::create([
-                        'user_id' => $user->id,
-                        'voucher_id' => $voucher->id,
-                        'amount' => $voucherPrice,
-                        'type' => "withdrawal",
-                        "creadit_balance" => ($balance - $voucherPrice),
-                        'description' => 'خرید ووچر و کسر مبلغ از کیف پول'
-                    ]);
-                    $payment_amount = $service->amount;
-                    return redirect()->route('panel.delivery')->with(['voucher' => $voucher, 'payment_amount' => $payment_amount]);
-
-
-                } else {
-                    $voucher->update([
-                        'status' => 'failed',
-                        'description' => "ارتباط با سروریس پرفکت مانی موفقیت آمیز بود. متن خطا ({$PMeVoucher['ERROR']})",
-                    ]);
-                    Log::emergency("perfectmoney error : " . $PMeVoucher['ERROR']);
-                    return redirect()->route('panel.purchase.view')->withErrors(['error' => "عملیات خرید ووچر ناموفق بود در صورت کسر موجودی از کیف پول شما با پشتیبانی تماس حاصل فرمایید."]);
-                }
-
-            } elseif (isset($inputs['custom_payment'])) {
-
-
-                $voucherPrice = $dollar->amount_to_rials * $inputs['custom_payment'];
-
-                if ($voucherPrice > $balance) {
-                    return redirect()->route('panel.purchase.view')->withErrors(['Low_inventory' => "موجودی کیف پول شما کافی نیست"]);
-                }
-                $inputs['final_amount'] = $voucherPrice;
-                $inputs['type'] = 'service';
-                $inputs['service_id_custom'] = $inputs['custom_payment'];
-
-                $invoice = Invoice::create($inputs);
-
-                $PM = new PerfectMoneyAPI(env('PM_ACCOUNT_ID'), env('PM_PASS'));
-
-                $PMeVoucher = $PM->createEV(env('PAYER_ACCOUNT'), $inputs['custom_payment']);
-                $voucher = Voucher::create(
-                    [
-                        'user_id' => $user->id,
-                        'invoice_id' => $invoice->id,
-                        'status' => 'requested',
-                        'description' => 'ارسال در خواست به سروریس پرفکت مانی',
-                        "service_id_custom" => $inputs['custom_payment']
-                    ]
-                );
-                if (is_array($PMeVoucher) and isset($PMeVoucher['VOUCHER_NUM']) and isset($PMeVoucher['VOUCHER_CODE'])) {
-                    $voucher->update([
-                        'status' => 'finished',
-                        'description' => 'ارتباط با سروریس پرفکت مانی موفقیت آمیز بود',
-                        "serial" => $PMeVoucher['VOUCHER_NUM'],
-                        'code' => $PMeVoucher['VOUCHER_CODE']
-                    ]);
-                    Log::emergency("panel Controller :" . json_encode($PMeVoucher));
-                    FinanceTransaction::create([
-                        'user_id' => $user->id,
-                        'voucher_id' => $voucher->id,
-                        'amount' => $voucherPrice,
-                        'type' => "withdrawal",
-                        "creadit_balance" => ($balance - $voucherPrice),
-                        'description' => 'خرید ووچر و کسر مبلغ از کیف پول'
-                    ]);
-                    $payment_amount = $inputs['custom_payment'];
-                    return redirect()->route('panel.delivery')->with(['voucher' => $voucher, 'payment_amount' => $payment_amount]);
-
-
-                } else {
-                    $voucher->update([
-                        'status' => 'failed',
-                        'description' => "ارتباط با سروریس پرفکت مانی موفقیت آمیز بود. متن خطا ({$PMeVoucher['ERROR']})",
-                    ]);
-                    Log::emergency("perfectmoney error : " . $PMeVoucher['ERROR']);
-                    return redirect()->route('panel.purchase.view')->withErrors(['error' => "عملیات خرید ووچر ناموفق بود در صورت کسر موجودی از کیف پول شما با پشتیبانی تماس حاصل فرمایید."]);
-                }
-
-
-            } else {
-                return redirect()->route('panel.purchase.view')->withErrors(['SelectInvalid' => "انتخاب شما معتبر نمیباشد"]);
-            }
-
-        } catch (\Exception $exception) {
-            return redirect()->route('panel.purchase.view')->withErrors(['error' => "عملیات خرید ووچر ناموفق بود در صورت کسر موجودی از کیف پول شما با پشتیبانی تماس حاصل فرمایید."]);
-        }
+        return $this->generateVoucher();
 
     }
 
@@ -195,40 +74,53 @@ class PanelController extends Controller
 
     public function walletCharging(WalletChargingRequest $request)
     {
+
         $dollar = Doller::orderBy('id', 'desc')->first();
-        $inputs=$request->all();
+        $inputs = $request->all();
+        $user = Auth::user();
+        $bank = Bank::find($inputs['bank']);
+        $inputs['user_id'] = $user->id;
+        $inputs['description'] = " شارژ کیف پول کاربر از طریق $bank->name";
 
         if (isset($inputs['service_id'])) {
             $service = Service::find($inputs['service_id']);
             $voucherPrice = $dollar->amount_to_rials * $service->amount;
-        }elseif (isset($inputs['custom_payment'])) {
+        } elseif (isset($inputs['custom_payment'])) {
+            $inputs['service_id_custom'] = $inputs['custom_payment'];
             $voucherPrice = $dollar->amount_to_rials * $inputs['custom_payment'];
-        }
-        else{
+        } else {
             return redirect()->route('panel.purchase.view')->withErrors(['SelectInvalid' => "انتخاب شما معتبر نمیباشد"]);
         }
-        $bank=Bank::find($inputs['bank']);
-        $objBank=new $bank->class;
+        $inputs['final_amount'] = $voucherPrice;
+        $inputs['type'] = 'wallet';
+        $invoice = Invoice::create($inputs);
+        $objBank = new $bank->class;
         $objBank->setTotalPrice($voucherPrice);
         $objBank->setBankUrl($bank->url);
         $objBank->setTerminalId($bank->terminal_id);
         $objBank->setUrlBack(route('panel.back.wallet.charging'));
-        $status=$objBank->payment();
-        if (!$status)
-        {
-            return redirect()->route('panel.purchase.view')->withErrors(['error'=>'ارتباط با بانک فراهم نشد لطفا چند دقیقه بعد تلاش فرماید.']);
+        Payment::create(
+            [
+                'bank_id' => $bank->id,
+                'invoice_id' => $invoice,
+                'amount' => $voucherPrice,
+                'state' => 'requested'
+            ]
+        );
+        $status = $objBank->payment();
+        if (!$status) {
+            return redirect()->route('panel.purchase.view')->withErrors(['error' => 'ارتباط با بانک فراهم نشد لطفا چند دقیقه بعد تلاش فرماید.']);
         }
-        $url=$objBank->getBankUrl();
-        $token=$status;
-        return view('welcome',compact('token','url'));
+        $url = $objBank->getBankUrl();
+        $token = $status;
+        return view('welcome', compact('token', 'url'));
 
     }
+
     public function backWalletCharging(Request $request)
     {
-        dd($request->all());
+
     }
-
-
 
 
 }
--- app/Http/Traits/HasConfig.php
+++ app/Http/Traits/HasConfig.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Traits;
 
+use App\Models\Doller;
+use App\Models\FinanceTransaction;
+use App\Models\Invoice;
+use App\Models\Service;
+use App\Models\Voucher;
+use AyubIRZ\PerfectMoneyAPI\PerfectMoneyAPI;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 trait HasConfig
 {
@@ -14,5 +21,131 @@ trait HasConfig
             } else return false;
         }
 
+    }
+    private function generateVoucher()
+    {
+
+        try {
+            $inputs = request()->all();
+            $dollar = Doller::orderBy('id', 'desc')->first();
+            $balance = Auth::user()->getCreaditBalance();
+            $user = Auth::user();
+            $inputs['user_id'] = $user->id;
+
+            if (isset($inputs['service_id'])) {
+                $service = Service::find($inputs['service_id']);
+
+                $voucherPrice = $dollar->amount_to_rials * $service->amount;
+
+                if ($voucherPrice > $balance) {
+                    return redirect()->route('panel.purchase.view')->withErrors(['Low_inventory' => "موجودی کیف پول شما کافی نیست"]);
+                }
+                $inputs['final_amount'] = $voucherPrice;
+                $inputs['type'] = 'service';
+                $invoice = Invoice::create($inputs);
+
+                $PM = new PerfectMoneyAPI(env('PM_ACCOUNT_ID'), env('PM_PASS'));
+
+                $PMeVoucher = $PM->createEV(env('PAYER_ACCOUNT'), $service->amount);
+                $voucher = Voucher::create(
+                    [
+                        'user_id' => $user->id,
+                        'service_id' => $inputs['service_id'],
+                        'invoice_id' => $invoice->id,
+                        'status' => 'requested',
+                        'description' => 'ارسال در خواست به سروریس پرفکت مانی'
+                    ]
+                );
+                if (is_array($PMeVoucher) and isset($PMeVoucher['VOUCHER_NUM']) and isset($PMeVoucher['VOUCHER_CODE'])) {
+                    $voucher->update([
+                        'status' => 'finished',
+                        'description' => 'ارتباط با سروریس پرفکت مانی موفقیت آمیز بود',
+                        "serial" => $PMeVoucher['VOUCHER_NUM'],
+                        'code' => $PMeVoucher['VOUCHER_CODE']
+                    ]);
+                    Log::emergency("panel Controller :" . json_encode($PMeVoucher));
+                    FinanceTransaction::create([
+                        'user_id' => $user->id,
+                        'voucher_id' => $voucher->id,
+                        'amount' => $voucherPrice,
+                        'type' => "withdrawal",
+                        "creadit_balance" => ($balance - $voucherPrice),
+                        'description' => 'خرید ووچر و کسر مبلغ از کیف پول'
+                    ]);
+                    $payment_amount = $service->amount;
+                    return redirect()->route('panel.delivery')->with(['voucher' => $voucher, 'payment_amount' => $payment_amount]);
+
+                } else {
+                    $voucher->update([
+                        'status' => 'failed',
+                        'description' => "ارتباط با سروریس پرفکت مانی موفقیت آمیز بود. متن خطا ({$PMeVoucher['ERROR']})",
+                    ]);
+                    Log::emergency("perfectmoney error : " . $PMeVoucher['ERROR']);
+                    return redirect()->route('panel.purchase.view')->withErrors(['error' => "عملیات خرید ووچر ناموفق بود در صورت کسر موجودی از کیف پول شما با پشتیبانی تماس حاصل فرمایید."]);
+                }
+
+            } elseif (isset($inputs['custom_payment'])) {
+
+
+                $voucherPrice = $dollar->amount_to_rials * $inputs['custom_payment'];
+
+                if ($voucherPrice > $balance) {
+                    return redirect()->route('panel.purchase.view')->withErrors(['Low_inventory' => "موجودی کیف پول شما کافی نیست"]);
+                }
+                $inputs['final_amount'] = $voucherPrice;
+                $inputs['type'] = 'service';
+                $inputs['service_id_custom'] = $inputs['custom_payment'];
+
+                $invoice = Invoice::create($inputs);
+
+                $PM = new PerfectMoneyAPI(env('PM_ACCOUNT_ID'), env('PM_PASS'));
+
+                $PMeVoucher = $PM->createEV(env('PAYER_ACCOUNT'), $inputs['custom_payment']);
+                $voucher = Voucher::create(
+                    [
+                        'user_id' => $user->id,
+                        'invoice_id' => $invoice->id,
+                        'status' => 'requested',
+                        'description' => 'ارسال در خواست به سروریس پرفکت مانی',
+                        "service_id_custom" => $inputs['custom_payment']
+                    ]
+                );
+                if (is_array($PMeVoucher) and isset($PMeVoucher['VOUCHER_NUM']) and isset($PMeVoucher['VOUCHER_CODE'])) {
+                    $voucher->update([
+                        'status' => 'finished',
+                        'description' => 'ارتباط با سروریس پرفکت مانی موفقیت آمیز بود',
+                        "serial" => $PMeVoucher['VOUCHER_NUM'],
+                        'code' => $PMeVoucher['VOUCHER_CODE']
+                    ]);
+                    Log::emergency("panel Controller :" . json_encode($PMeVoucher));
+                    FinanceTransaction::create([
+                        'user_id' => $user->id,
+                        'voucher_id' => $voucher->id,
+                        'amount' => $voucherPrice,
+                        'type' => "withdrawal",
+                        "creadit_balance" => ($balance - $voucherPrice),
+                        'description' => 'خرید ووچر و کسر مبلغ از کیف پول'
+                    ]);
+                    $payment_amount = $inputs['custom_payment'];
+                    return redirect()->route('panel.delivery')->with(['voucher' => $voucher, 'payment_amount' => $payment_amount]);
+
+
+                } else {
+                    $voucher->update([
+                        'status' => 'failed',
+                        'description' => "ارتباط با سروریس پرفکت مانی موفقیت آمیز بود. متن خطا ({$PMeVoucher['ERROR']})",
+                    ]);
+                    Log::emergency("perfectmoney error : " . $PMeVoucher['ERROR']);
+                    return redirect()->route('panel.purchase.view')->withErrors(['error' => "عملیات خرید ووچر ناموفق بود در صورت کسر موجودی از کیف پول شما با پشتیبانی تماس حاصل فرمایید."]);
+                }
+
+
+            } else {
+                return redirect()->route('panel.purchase.view')->withErrors(['SelectInvalid' => "انتخاب شما معتبر نمیباشد"]);
+            }
+
+        } catch (\Exception $exception) {
+            return redirect()->route('panel.purchase.view')->withErrors(['error' => "عملیات خرید ووچر ناموفق بود در صورت کسر موجودی از کیف پول شما با پشتیبانی تماس حاصل فرمایید."]);
+        }
     }
 }
