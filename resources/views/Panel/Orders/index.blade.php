ac6f7f5c6b8a3cc55d01bba92b8530aa34c9cba7
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-07-31 14:30:58 +0330

    run demo order

--- resources/views/Panel/Orders/index.blade.php
+++ resources/views/Panel/Orders/index.blade.php
@@ -5,10 +5,32 @@
 
 
     <section class="mx-auto px-4">
-         <article class="border-2 rounded-md border-white p-2">
+         <article class="border-2 rounded-md border-white p-2 space-y-3.5">
              <div class="text-sm sm:text-base flex items-center justify-between">
                  <p>شماره تراکنش: 5489</p>
                  <p class="text-center">مبلغ کل:500000 ریال</p>
+             </div>
+             <div class="text-sm sm:text-base flex items-center justify-between">
+                 <div class="flex items-center space-x-2 space-x-reverse">
+                 <img src="{{asset('src/images/prl.png')}}" alt="">
+                     <p>خرید ووچر پرفکت مانی 2 دلاری</p>
+                 </div>
+                 <p class="text-center">
+                     وضعیت:
+                     <span class="text-green-400">انجام شده</span>
+                 </p>
+             </div>
+
+             <div class="text-sm sm:text-base flex items-center justify-between">
+                 <div class="flex items-center space-x-2 space-x-reverse">
+
+                     <p>تاریخ ثبت سفارش:</p>
+                     <p>1403/5/10  11:50</p>
+                 </div>
+                 <a href="" class="bg-gray-100 px-4 py-1.5 rounded-md space-x-3">
+                     <i class="fa-solid fa-eye text-black"></i>
+                     <span class="text-black">مشاهده جزئیات</span>
+                 </a>
              </div>
          </article>
     </section>
