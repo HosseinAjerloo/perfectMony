bca49444f621f5eca9920c5cd2b5b94336e6995f
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-07-31 14:34:51 +0330

    run demo order

--- resources/views/Panel/Orders/index.blade.php
+++ resources/views/Panel/Orders/index.blade.php
@@ -29,7 +29,7 @@
                  </div>
                  <a href="" class="bg-gray-100 px-4 py-1.5 rounded-md space-x-3">
                      <i class="fa-solid fa-eye text-black"></i>
-                     <span class="text-black">جزئیات خرید</span>
+                     <span class="text-black">جزئیات</span>
                  </a>
              </div>
          </article>
