bb2405adf3eeb3ccca8ec3cd91a63c152a717982
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-08-01 12:54:17 +0330

    sync project

--- resources/views/Panel/Orders/details.blade.php
+++ resources/views/Panel/Orders/details.blade.php
@@ -3,78 +3,77 @@
 
 @section('container')
 
-
     <section class="mx-auto px-4 sm:w-3/4 md:w-3/5 lg:w-2/5 space-y-5">
 
-            <article class="border-2 rounded-md border-white p-3 space-y-3.5">
-                <div class="text-sm sm:text-base flex items-center justify-between">
-                    <p>شماره سفارش: {{$financeTransaction->id}}</p>
-                    <p class="text-center">مبلغ کل:{{$financeTransaction->amount}} ریال</p>
+        <article class="border-2 rounded-md border-white p-3 space-y-3.5">
+            <div class="text-sm sm:text-base flex items-center justify-between">
+                <p>شماره سفارش: {{$financeTransaction->id}}</p>
+                <p class="text-center">مبلغ کل:{{$financeTransaction->amount}} ریال</p>
+            </div>
+            <div class="text-sm sm:text-base flex items-center justify-between">
+                <div class="flex items-center space-x-2 space-x-reverse">
+                    <img src="{{asset('src/images/prl.png')}}" alt="">
+                    <p>خرید ووچر پرفکت مانی {{$financeTransaction->voucher->voucherAmount()}} دلاری</p>
                 </div>
-                <div class="text-sm sm:text-base flex items-center justify-between">
-                    <div class="flex items-center space-x-2 space-x-reverse">
-                        <img src="{{asset('src/images/prl.png')}}" alt="">
-                        <p>خرید ووچر پرفکت مانی {{$financeTransaction->voucher->voucherAmount()}} دلاری</p>
-                    </div>
-                    <p class="text-center">
-                        نرخ دلار:
-                        {{numberFormat($dollar->amount_to_rials)}}
-                        ریال
-                    </p>
-                </div>
+                <p class="text-center">
+                    نرخ دلار:
+                    {{numberFormat($dollar->amount_to_rials)}}
+                    ریال
+                </p>
+            </div>
 
-                <div class="text-sm sm:text-base flex items-center justify-between">
-                    <div class="flex items-center space-x-2 space-x-reverse">
+            <div class="text-sm sm:text-base flex items-center justify-between">
+                <div class="flex items-center space-x-2 space-x-reverse">
 
-                        <p>تاریخ ثبت سفارش:</p>
-                        <p>{{\Morilog\Jalali\Jalalian::forge($financeTransaction->created_at)->format('%A, %d %B %y')}}</p>
-                    </div>
-                    <p class="text-center">
-                        وضعیت:
-                        @if($financeTransaction->status=='success')
-                            <span class="text-green-400">انجام شده</span>
-                        @else
-                            <span class="text-red-700">درحال پردازش</span>
-                        @endif
-                    </p>
+                    <p>تاریخ ثبت سفارش:</p>
+                    <p>{{\Morilog\Jalali\Jalalian::forge($financeTransaction->created_at)->format('%A, %d %B %y')}}</p>
                 </div>
-                <div class="text-sm sm:text-base flex items-center space-x-2 space-x-reverse">
+                <p class="text-center">
+                    وضعیت:
                     @if($financeTransaction->status=='success')
-                    <img src="{{asset('src/images/Group 414.png')}}" alt="" class="h-8 w-8">
+                        <span class="text-green-400">انجام شده</span>
                     @else
-                        <img src="{{asset('src/images/warning.png')}}" alt="" class="h-4 w-6">
+                        <span class="text-red-700">درحال پردازش</span>
                     @endif
-                    <p class="mb-2">{{$financeTransaction->description}}</p>
-                </div>
+                </p>
+            </div>
+            <div class="text-sm sm:text-base flex items-center space-x-2 space-x-reverse">
+                @if($financeTransaction->status=='success')
+                    <img src="{{asset('src/images/Group 414.png')}}" alt="" class="h-8 w-8">
+                @else
+                    <img src="{{asset('src/images/warning.png')}}" alt="" class="h-4 w-6">
+                @endif
+                <p class="mb-2">{{$financeTransaction->description}}</p>
+            </div>
 
-                <div class="px-3 py-1.5 bg-white rounded-md text-black text-sm sm:text-base">
-                    <div class="flex items-center space-x-16 space-x-reverse">
-                        <p class="text-sm sm:text-base font-semibold">شماره ووچر:</p>
-                        <div class=" items-center space-x-4 space-x-reverse text-sm sm:text-base font-semibold">
-                            <img src="{{asset('src/images/copy.svg')}}" alt="" class="inline-block copy">
-                            <span  class="inline-block"> {{$financeTransaction->voucher->serial}}</span>
-                        </div>
+            <div class="px-3 py-1.5 bg-white rounded-md text-black text-sm sm:text-base">
+                <div class="flex items-center space-x-16 space-x-reverse">
+                    <p class="text-sm sm:text-base font-semibold">شماره ووچر:</p>
+                    <div class=" items-center space-x-4 space-x-reverse text-sm sm:text-base font-semibold">
+                        <img src="{{asset('src/images/copy.svg')}}" alt="" class="inline-block copy">
+                        <span class="inline-block"> {{$financeTransaction->voucher->serial}}</span>
                     </div>
                 </div>
-                <div class="px-3 py-1.5 bg-white rounded-md text-black text-sm sm:text-base">
-                    <div class="flex items-center space-x-16 space-x-reverse">
-                        <p class="text-sm sm:text-base font-semibold">کد فعال سازی:</p>
-                        <div class=" items-center space-x-4 space-x-reverse text-sm sm:text-base font-semibold">
-                            <img src="{{asset('src/images/copy.svg')}}" alt="" class="inline-block copy">
-                            <span  class="inline-block">{{$financeTransaction->voucher->code}}</span>
-                        </div>
+            </div>
+            <div class="px-3 py-1.5 bg-white rounded-md text-black text-sm sm:text-base">
+                <div class="flex items-center space-x-16 space-x-reverse">
+                    <p class="text-sm sm:text-base font-semibold">کد فعال سازی:</p>
+                    <div class=" items-center space-x-4 space-x-reverse text-sm sm:text-base font-semibold">
+                        <img src="{{asset('src/images/copy.svg')}}" alt="" class="inline-block copy">
+                        <span class="inline-block">{{$financeTransaction->voucher->code}}</span>
                     </div>
                 </div>
-                <div class="px-3 py-1.5 bg-white rounded-md text-black text-sm sm:text-base">
-                    <div class="flex items-center space-x-16 space-x-reverse">
-                        <p class="text-sm sm:text-base font-semibold">شماره سفارش:</p>
-                        <div class=" items-center space-x-4 space-x-reverse text-sm sm:text-base font-semibold">
-                            <img src="{{asset('src/images/copy.svg')}}" alt="" class="inline-block copy">
-                            <span  class="inline-block">{{$financeTransaction->id}}</span>
-                        </div>
+            </div>
+            <div class="px-3 py-1.5 bg-white rounded-md text-black text-sm sm:text-base">
+                <div class="flex items-center space-x-16 space-x-reverse">
+                    <p class="text-sm sm:text-base font-semibold">شماره سفارش:</p>
+                    <div class=" items-center space-x-4 space-x-reverse text-sm sm:text-base font-semibold">
+                        <img src="{{asset('src/images/copy.svg')}}" alt="" class="inline-block copy">
+                        <span class="inline-block">{{$financeTransaction->id}}</span>
                     </div>
                 </div>
-            </article>
+            </div>
+        </article>
 
     </section>
 
@@ -87,25 +86,24 @@
 
         function copyToClipboard(text) {
 
-            var textArea = document.createElement( "textarea" );
+            var textArea = document.createElement("textarea");
             textArea.value = text;
-            document.body.appendChild( textArea );
+            document.body.appendChild(textArea);
             textArea.select();
 
             try {
-                var successful = document.execCommand( 'copy' );
+                var successful = document.execCommand('copy');
                 var msg = successful ? 'successful' : 'unsuccessful';
                 console.log('Copying text command was ' + msg);
             } catch (err) {
-                console.log('Oops, unable to copy',err);
+                console.log('Oops, unable to copy', err);
             }
-            document.body.removeChild( textArea );
+            document.body.removeChild(textArea);
         }
 
-        $( '.copy' ).click( function()
-        {
-             let spanText=$(this).siblings('span').text();
-             copyToClipboard( spanText);
+        $('.copy').click(function () {
+            let spanText = $(this).siblings('span').text();
+            copyToClipboard(spanText);
         });
     </script>
 
