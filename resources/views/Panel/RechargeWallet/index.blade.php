f4c0a09b8c91b72b9731a02fa19e40e9acae6509
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-08-26 17:19:56 +0330

    change view wallet charging

--- resources/views/Panel/RechargeWallet/index.blade.php
+++ resources/views/Panel/RechargeWallet/index.blade.php
@@ -1,14 +1,21 @@
 @extends('Panel.layout.master')
 
 @section('message-box')
-    <div class="flex items-center justify-center space-x-reverse space-x-4 bg-gray-500 p-2">
-        <img src="{{asset('src/images/dollar 1.png')}}" alt="" class="w-9 h-9">
-        <p class="text-sm text-center sm:text-base">موجودی فعلی شما:{{$balance}} ریال میباشد</p>
+    <div class="flex  p-2 w-full items-center justify-center">
+        <div
+            class="rounded-md border-2 border-white w-full p-2 flex space-x-3 space-x-reverse items-center sm:w-1/2 md:w-1/3">
+            <img src="{{asset('src/images/newWallet.svg')}}" alt="" class="w-8 h-8">
+            <h1 class="font-bold text-white text-md mt-1">کیف پول شما {{$balance}} ریال است</h1>
+        </div>
     </div>
 @endsection
 
 @section('container')
-
+    <div class="flex items-center justify-center flex-col space-y-3  w-full sm:w-1/2 mx-auto text-center p-2">
+        <p>مناسب کسانی که خرید های مکرر از ساینا ارز دارند.</p>
+        <p>1-شارژ کیف پول بالای 10 دلار یک دلار اضافه شارژ می شود</p>
+        <p>2-موضع تسویه کارت هدیه و یا حواله کیف پول را انتخاب می کنید</p>
+    </div>
     <form class="flex items-center justify-center flex-col space-y-3  w-full sm:w-1/2 mx-auto"
           action="{{route('panel.wallet.charging-Preview')}}" method="get" id="form">
         <p class="text-center text-sm py-2 sm:text-base ">
@@ -36,11 +43,11 @@
 @section('script-tag')
 
     <script>
-        $(document).ready(function (){
-           $(".bank").click(function (){
+        $(document).ready(function () {
+            $(".bank").click(function () {
                 $("#form").submit();
 
-           }) ;
+            });
         });
     </script>
 @endsection
