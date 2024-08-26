544b18943f1a5a852a57ae8c8bee27e557c24083
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-08-26 16:45:09 +0330

    change view project

--- resources/views/Panel/index.blade.php
+++ resources/views/Panel/index.blade.php
@@ -28,63 +28,61 @@
             </a>
         </div>
 
-        <div class="flex justify-center items-center flex-col  w-full sm:w-2/6 md:w-1/3 lg:w-1/4 space-y-5">
-            <div class="border-2 border-2-white rounded-md py-3 px-3  w-full text-center   bg-gray-800">
-                <a href="{{route('panel.purchase.view')}}"
-                   class="text-center flex items-center space-x-2 space-x-reverse">
-                    <img src="{{asset('src/images/prl.png')}}" alt="" class="w-6 h-6">
-                    <p>کارت هدیه پرفکت مانی</p>
-                </a>
-            </div>
-            <div class="border-2 border-2-white rounded-md py-3 px-3  w-full text-center   bg-gray-800">
-                <a href="{{route('panel.transmission.view')}}"
-                   class="text-center flex items-center space-x-2 space-x-reverse">
-                    <i class="fa-solid fa-money-bill-transfer w-6 h-6"></i>
-                    <p>حواله پرفکت</p>
-                </a>
-            </div>
-            <div class="border-2 border-2-white rounded-md py-3 px-3  w-full text-center   bg-gray-800">
-
-                <a href="{{route('panel.order.expectation')}}"
-                   class="text-center flex items-center space-x-2 space-x-reverse">
-                    <img src="{{asset('src/images/invoice.svg')}}" alt="" class="w-6 h-6">
-                    <p>سفارشات شما</p>
-                </a>
-            </div>
-
-            <div class="border-2 border-2-white rounded-md py-3 px-3  w-full text-center   bg-gray-800">
-                <a href="{{route('panel.order')}}" class="text-center flex items-center space-x-2 space-x-reverse">
-                    <img src="{{asset('src/images/mony.png')}}" alt="" class="w-6 h-6">
-                    <p>سوابق مالی</p>
-                </a>
-            </div>
-            <div class="border-2 border-2-white rounded-md py-3 px-3 w-full text-center   bg-gray-800">
-                <a href="{{route('panel.ticket')}}" class="text-center flex items-center space-x-2 space-x-reverse">
-                    <img src="{{asset('src/images/ticket.png')}}" alt="" class="w-6 h-6">
-                    <p>تیکت پشتیبانی</p>
-                </a>
-            </div>
-            <div class="border-2 border-2-white rounded-md py-3 px-3 w-full text-center   bg-gray-800">
-                <a href="{{route('panel.contactUs')}}" class="text-center flex items-center space-x-2 space-x-reverse">
-                    <img src="{{asset('src/images/phone.png')}}" alt="" class="w-6 h-6">
-                    <p>تماس با ما</p>
-                </a>
-            </div>
-            @if(\Illuminate\Support\Facades\Auth::user()->type=='admin')
-                <div class="border-2 border-2-white rounded-md py-3 px-3 w-full text-center   bg-gray-800">
-                    <a href="{{route('panel.admin')}}" class="text-center flex items-center space-x-2 space-x-reverse">
-                        <i class="fa-solid fa-user w-6 h-6"></i>
-                        <p>ورود به پنل ادمین</p>
-                    </a>
+        <div class="flex justify-between items-center   w-full  flex-wrap ">
+            <a href="{{route('panel.order')}}" class="flex flex-col items-center justify-center space-y-2 mb-7 w-1/3 sm:w-1/4 md:w-1/6">
+                <div class="border-white border-2 rounded-full p-4 box-border">
+                    <img src="{{asset('src/images/newTransaction.svg')}}" alt="">
                 </div>
+                <p class="text-sm sm:text-base"> سوابق</p>
+            </a>
+            <a href="{{route('panel.transmission.view')}}" class="flex flex-col items-center justify-center space-y-2  mb-7 w-1/3 sm:w-1/4 md:w-1/6">
+                <div class="border-white border-2 rounded-full p-4 box-border">
+                    <img src="{{asset('src/images/newPm.svg')}}" alt="" >
+                </div>
+                <p class="text-sm sm:text-base">حواله پرفکت مانی</p>
+            </a>
+            <a href="{{route('panel.purchase.view')}}"  class="flex flex-col items-center justify-center space-y-2  mb-7 w-1/3 sm:w-1/4 md:w-1/6">
+                <div class="border-white border-2 rounded-full p-4 box-border">
+                    <img src="{{asset('src/images/newGift.svg')}}" alt="">
+                </div>
+                <p class="text-sm sm:text-base">کارت هدیه</p>
+            </a>
+            <a href="{{route('panel.wallet.charging')}}" class="flex flex-col items-center justify-center space-y-2  mb-7 w-1/3 sm:w-1/4 md:w-1/6">
+                <div class="border-white border-2 rounded-full p-4 box-border">
+                    <img src="{{asset('src/images/newWallet.svg')}}" alt="">
+                </div>
+                <p class="text-sm sm:text-base">کیف پول</p>
+            </a>
+            <a href="{{route('panel.contactUs')}}" class="flex flex-col items-center justify-center space-y-2  mb-7 w-1/3 sm:w-1/4 md:w-1/6">
+                <div class="border-white border-2 rounded-full p-4 box-border">
+                    <img src="{{asset('src/images/newPhone.svg')}}" alt="">
+                </div>
+                <p class="text-sm sm:text-base">تماس باما</p>
+            </a>
+            <a href="{{route('panel.ticket')}}" class="flex flex-col items-center justify-center space-y-2  mb-7 w-1/3 sm:w-1/4 md:w-1/6">
+                <div class="border-white border-2 rounded-full p-4 box-border">
+                    <img src="{{asset('src/images/newTicket.svg')}}" alt="">
+                </div>
+                <p class="text-sm sm:text-base">پشتیبانی</p>
+            </a>
+            @if(\Illuminate\Support\Facades\Auth::user()->type=='admin')
+
+            <a href="{{route('panel.admin')}}"  class="flex flex-col items-center justify-center space-y-2  mb-7 w-1/3 sm:w-1/4 md:w-1/6">
+                <div class="border-white border-2 rounded-full p-4 box-border">
+                    <img src="{{asset('src/images/newAdmin.svg')}}" alt="">
+                </div>
+                <p class="text-sm sm:text-base">ورود به پنل ادمین</p>
+            </a>
             @endif
 
         </div>
 
 
         @if(session()->has('previous_user') and session()->get('previous_user')!=\Illuminate\Support\Facades\Auth::user()->id)
-            <div class="bg-rose-500 py-1.5 px-2 rounded-md font-semibold notifyBox shadow-2xl shadow-white z-[100] sm:left-5 sm:bottom-8 sm:absolute ">
-                <a  href="{{route('panel.admin.login-another-user',session()->get('previous_user'))}}" class="text-sm sm:text-base">ورود به حساب پشتیبان</a>
+            <div
+                class="bg-rose-500 py-1.5 px-2 rounded-md font-semibold notifyBox shadow-2xl shadow-white z-[100] sm:left-5 sm:bottom-8 sm:absolute ">
+                <a href="{{route('panel.admin.login-another-user',session()->get('previous_user'))}}"
+                   class="text-sm sm:text-base">ورود به حساب پشتیبان</a>
             </div>
         @endif
     </article>
