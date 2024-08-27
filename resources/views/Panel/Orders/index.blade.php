2e4614429a81cf4f5edba44b997708aa7304af8a
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-08-27 14:46:19 +0330

    sync project

--- resources/views/Panel/Orders/index.blade.php
+++ resources/views/Panel/Orders/index.blade.php
@@ -13,126 +13,188 @@
                 <div class="text-[11.5px] w-2/12  text-center py-2">مانده هزارتومان</div>
             </div>
         </div>
-        <article class="min-w-full w-full ">
-            <div class="min-w-full w-full flex flex-col bg-slate-50 text-black p-2 rounded-md">
-                <div class="flex items-center min-w-full  justify-between ">
-                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>
-                    <div class="text-[11.5px] w-1/4  text-center py-2">صدور کارت هدیه پرفکت مانی</div>
-                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>
-                    <div class="text-[11.5px] w-1/12  text-center py-2"><i
-                            class="fa-solid fa-minus text-[11.5px] text-rose-500"></i></div>
-                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>
-                </div>
-                <div class="flex items-center justify-between">
-                    <p class="text-sm font-semibold sm:text-base">
-                        کد فعال سازی:
-                    </p>
-                    <div class="flex items-center space-x-3 space-x-reverse">
-                        <span class="text-sm sm:text-base">100001515645</span>
-                        <img src="{{asset('src/images/Group 422.png')}}" alt="" class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
-                    </div>
-                </div>
-                <div class="flex items-center justify-between">
-                    <p class="text-sm font-semibold sm:text-base">
-                        شماره ووچر:
-                    </p>
-                    <div class=" relative">
-                        <div class="flex items-center space-x-3 space-x-reverse">
-                            <span class="text-sm sm:text-base">465465</span>
-                            <img src="{{asset('src/images/Group 422.png')}}" alt="" class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
+        @foreach($financeTransactions as $financeTransaction)
+            <article class="min-w-full w-full ">
+                <div class="min-w-full w-full flex flex-col bg-slate-50 text-black p-2 rounded-md">
+                    <div class="flex items-center min-w-full  justify-between ">
+                        <div
+                            class="text-[11.5px] w-2/12  text-center py-2">{{\Morilog\Jalali\Jalalian::forge($financeTransaction->created_at)->format('Y/m/d H:i:s')}}</div>
+                        <div
+                            class="text-[11.5px] w-1/4  text-center py-2">{{$financeTransaction->description??''}}</div>
+                        <div class="text-[11.5px] w-2/12 text-center py-2">
+                            {{ strrev(substr(strrev($financeTransaction->amount),4))!=""?strrev(substr(strrev($financeTransaction->amount),4)):0}}
                         </div>
+                        <div class="text-[11.5px] w-1/12  text-center py-2">
+                            @if($financeTransaction->type=="deposit")
+                                <i class="fa-solid fa-plus text-[11.5px] text-green-400"></i>
 
-                    </div>
-                </div>
-            </div>
-
-        </article>
-        <article class="min-w-full w-full ">
-            <div class="min-w-full w-full flex flex-col bg-slate-200 text-black p-2 rounded-md">
-                <div class="flex items-center min-w-full  justify-between ">
-                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>
-                    <div class="text-[11.5px] w-1/4  text-center py-2">صدور کارت هدیه پرفکت مانی</div>
-                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>
-                    <div class="text-[11.5px] w-1/12  text-center py-2">
-                        <i class="fa-solid fa-plus text-[11.5px] text-green-400"></i>
-                    </div>
-                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>
-                </div>
-
-
-            </div>
-
-        </article>
-
-        <article class="min-w-full w-full ">
-            <div class="min-w-full w-full flex flex-col bg-slate-50 text-black p-2 rounded-md">
-                <div class="flex items-center min-w-full  justify-between ">
-                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>
-                    <div class="text-[11.5px] w-1/4  text-center py-2">صدور کارت هدیه پرفکت مانی</div>
-                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>
-                    <div class="text-[11.5px] w-1/12  text-center py-2"><i
-                            class="fa-solid fa-minus text-[11.5px] text-rose-500"></i></div>
-                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>
-                </div>
-                <div class="flex items-center justify-between">
-                    <p class="text-sm font-semibold sm:text-base">
-                        کد فعال سازی:
-                    </p>
-                    <div class="flex items-center space-x-3 space-x-reverse">
-                        <span class="text-sm sm:text-base">100001515645</span>
-                        <img src="{{asset('src/images/Group 422.png')}}" alt="" class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
-                    </div>
-                </div>
-                <div class="flex items-center justify-between">
-                    <p class="text-sm font-semibold sm:text-base">
-                        شماره ووچر:
-                    </p>
-                    <div class=" relative">
-                        <div class="flex items-center space-x-3 space-x-reverse">
-                            <span class="text-sm sm:text-base">465465</span>
-                            <img src="{{asset('src/images/Group 422.png')}}" alt="" class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
+                            @elseif($financeTransaction->type=="withdrawal")
+                                <i class="fa-solid fa-minus text-[11.5px] text-rose-500"></i>
+                            @else
+                                <i class="fas fa-times-circle text-[11.5px] text-rose-500"></i>
+                            @endif
                         </div>
-
+                        <div class="text-[11.5px] w-2/12  text-center py-2">
+                            {{strrev(substr(strrev($financeTransaction->creadit_balance),4))!=""?strrev(substr(strrev($financeTransaction->creadit_balance),4)):0}}
+                        </div>
                     </div>
-                </div>
-            </div>
+                    @if($financeTransaction->voucher)
 
-        </article>
-        <article class="min-w-full w-full ">
-            <div class="min-w-full w-full flex flex-col bg-slate-200 text-black p-2 rounded-md">
-                <div class="flex items-center min-w-full  justify-between ">
-                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>
-                    <div class="text-[11.5px] w-1/4  text-center py-2">افزایش مبلغ کیف پول درگاه بانک سامان</div>
-                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>
-                    <div class="text-[11.5px] w-1/12  text-center py-2">
-                        <i class="fa-solid fa-plus text-[11.5px] text-green-400"></i>
+
+                    <div class="flex items-center justify-between">
+                        <p class="text-sm font-semibold sm:text-base">
+                            کد فعال سازی:
+                        </p>
+                        <div class="flex items-center space-x-3 space-x-reverse">
+                            <span class="text-sm sm:text-base">{{$financeTransaction->voucher->code}}</span>
+                            <img src="{{asset('src/images/Group 422.png')}}" alt=""
+                                 class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
+                        </div>
                     </div>
-                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>
-                </div>
+                    <div class="flex items-center justify-between">
+                        <p class="text-sm font-semibold sm:text-base">
+                            شماره ووچر:
+                        </p>
+                        <div class=" relative">
+                            <div class="flex items-center space-x-3 space-x-reverse">
+                                <span class="text-sm sm:text-base">{{$financeTransaction->voucher->serial}}</span>
+                                <img src="{{asset('src/images/Group 422.png')}}" alt=""
+                                     class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
+                            </div>
 
-
-            </div>
-
-        </article>
-
-        <article class="min-w-full w-full ">
-            <div class="min-w-full w-full flex flex-col bg-slate-200 text-black p-2 rounded-md">
-                <div class="flex items-center min-w-full  justify-between ">
-                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>
-                    <div class="text-[11.5px] w-1/4  text-center py-2">افزایش مبلغ کیف پول درگاه بانک سامان</div>
-                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>
-                    <div class="text-[11.5px] w-1/12  text-center py-2">
-                        <i class="fas fa-times-circle text-[11.5px] text-rose-500"></i>
+                        </div>
                     </div>
-                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>
+                    @endif
+
+
+                    @if($financeTransaction->transmission)
+
+
+                        <div class="flex items-center justify-between">
+                            <p class="text-sm font-semibold sm:text-base">
+                                مبلغ انتقال:
+                            </p>
+                            <div class="flex items-center space-x-3 space-x-reverse">
+                                <span class="text-sm sm:text-base">{{$financeTransaction->transmission->payment_amount}}</span>
+                                <img src="{{asset('src/images/Group 422.png')}}" alt=""
+                                     class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
+                            </div>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <p class="text-sm font-semibold sm:text-base">
+                                شماره حواله:
+                            </p>
+                            <div class="flex items-center space-x-3 space-x-reverse">
+                                <span class="text-sm sm:text-base">{{$financeTransaction->transmission->payment_batch_num}}</span>
+                                <img src="{{asset('src/images/Group 422.png')}}" alt=""
+                                     class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
+                            </div>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <p class="text-sm font-semibold sm:text-base">
+                                شماره حساب گیرنده:
+                            </p>
+                            <div class=" relative">
+                                <div class="flex items-center space-x-3 space-x-reverse">
+                                    <span class="text-sm sm:text-base">{{$financeTransaction->transmission->payee_account}}</span>
+                                    <img src="{{asset('src/images/Group 422.png')}}" alt=""
+                                         class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">
+                                </div>
+
+                            </div>
+                        </div>
+                    @endif
                 </div>
-                <p class="text-[11.5px] leading-6">
-                    پرداخت موفقیت آمیز نبود انصراف کاربر از درگاه پرداخت  پرداخت موفقیت آمیز نبود انصراف کاربر از درگاه پرداخت  پرداخت موفقیت آمیز نبود انصراف کاربر از درگاه پرداخت  پرداخت موفقیت آمیز نبود انصراف کاربر از درگاه پرداخت
-                </p>
 
-            </div>
+            </article>
 
-        </article>
+        @endforeach
+{{--        <article class="min-w-full w-full ">--}}
+{{--            <div class="min-w-full w-full flex flex-col bg-slate-200 text-black p-2 rounded-md">--}}
+{{--                <div class="flex items-center min-w-full  justify-between ">--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>--}}
+{{--                    <div class="text-[11.5px] w-1/4  text-center py-2">صدور کارت هدیه پرفکت مانی</div>--}}
+{{--                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>--}}
+{{--                    <div class="text-[11.5px] w-1/12  text-center py-2">--}}
+{{--                        <i class="fa-solid fa-plus text-[11.5px] text-green-400"></i>--}}
+{{--                    </div>--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>--}}
+{{--                </div>--}}
+
+
+{{--            </div>--}}
+
+{{--        </article>--}}
+
+{{--        <article class="min-w-full w-full ">--}}
+{{--            <div class="min-w-full w-full flex flex-col bg-slate-50 text-black p-2 rounded-md">--}}
+{{--                <div class="flex items-center min-w-full  justify-between ">--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>--}}
+{{--                    <div class="text-[11.5px] w-1/4  text-center py-2">صدور کارت هدیه پرفکت مانی</div>--}}
+{{--                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>--}}
+{{--                    <div class="text-[11.5px] w-1/12  text-center py-2"><i--}}
+{{--                            class="fa-solid fa-minus text-[11.5px] text-rose-500"></i></div>--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>--}}
+{{--                </div>--}}
+{{--                <div class="flex items-center justify-between">--}}
+{{--                    <p class="text-sm font-semibold sm:text-base">--}}
+{{--                        کد فعال سازی:--}}
+{{--                    </p>--}}
+{{--                    <div class="flex items-center space-x-3 space-x-reverse">--}}
+{{--                        <span class="text-sm sm:text-base">100001515645</span>--}}
+{{--                        <img src="{{asset('src/images/Group 422.png')}}" alt=""--}}
+{{--                             class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="flex items-center justify-between">--}}
+{{--                    <p class="text-sm font-semibold sm:text-base">--}}
+{{--                        شماره ووچر:--}}
+{{--                    </p>--}}
+{{--                    <div class=" relative">--}}
+{{--                        <div class="flex items-center space-x-3 space-x-reverse">--}}
+{{--                            <span class="text-sm sm:text-base">465465</span>--}}
+{{--                            <img src="{{asset('src/images/Group 422.png')}}" alt=""--}}
+{{--                                 class="w-6 h-6 mt-1 copy cursor-pointer transition-all hover:scale-150">--}}
+{{--                        </div>--}}
+
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+
+{{--        </article>--}}
+{{--        <article class="min-w-full w-full ">--}}
+{{--            <div class="min-w-full w-full flex flex-col bg-slate-200 text-black p-2 rounded-md">--}}
+{{--                <div class="flex items-center min-w-full  justify-between ">--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>--}}
+{{--                    <div class="text-[11.5px] w-1/4  text-center py-2">افزایش مبلغ کیف پول درگاه بانک سامان</div>--}}
+{{--                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>--}}
+{{--                    <div class="text-[11.5px] w-1/12  text-center py-2">--}}
+{{--                        <i class="fa-solid fa-plus text-[11.5px] text-green-400"></i>--}}
+{{--                    </div>--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>--}}
+{{--                </div>--}}
+
+
+{{--            </div>--}}
+
+{{--        </article>--}}
+
+{{--        <article class="min-w-full w-full ">--}}
+{{--            <div class="min-w-full w-full flex flex-col bg-slate-200 text-black p-2 rounded-md">--}}
+{{--                <div class="flex items-center min-w-full  justify-between ">--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">1403/02/01 11:07</div>--}}
+{{--                    <div class="text-[11.5px] w-1/4  text-center py-2">افزایش مبلغ کیف پول درگاه بانک سامان</div>--}}
+{{--                    <div class="text-[11.5px] w-2/12 text-center py-2">120،000</div>--}}
+{{--                    <div class="text-[11.5px] w-1/12  text-center py-2">--}}
+{{--                        <i class="fas fa-times-circle text-[11.5px] text-rose-500"></i>--}}
+{{--                    </div>--}}
+{{--                    <div class="text-[11.5px] w-2/12  text-center py-2">120،000</div>--}}
+{{--                </div>--}}
+{{--              --}}
+
+{{--            </div>--}}
+
+{{--        </article>--}}
     </section>
 
 @endsection
@@ -141,5 +203,29 @@
 
 @section('script-tag')
 
+    <script>
 
+        function copyToClipboard(text) {
+
+            var textArea = document.createElement( "textarea" );
+            textArea.value = text;
+            document.body.appendChild( textArea );
+            textArea.select();
+
+            try {
+                var successful = document.execCommand( 'copy' );
+                var msg = successful ? 'successful' : 'unsuccessful';
+                console.log('Copying text command was ' + msg);
+            } catch (err) {
+                console.log('Oops, unable to copy',err);
+            }
+            document.body.removeChild( textArea );
+        }
+
+        $( '.copy' ).click( function()
+        {
+            let spanText=$(this).siblings('span').text();
+            copyToClipboard( spanText);
+        });
+    </script>
 @endsection
