c591429529a8e5cc2d1adcedb430758c481eed6b
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-09-11 09:52:17 +0330

    add view email

--- resources/views/components/email-failed.blade.php
+++ resources/views/components/email-failed.blade.php
@@ -1,3 +1,51 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+@switch($type)
+    @case('message-box')
+        <section class="space-y-3">
+            <div class="flex justify-center min-w-full items-center">
+                <img src="{{asset('src/images/faildIcon.svg')}}" class="w-8 h-8">
+                <h1 class="font-bold text-lg text-rose-500">خرید ناموفق کارت هدیه (ووچر)</h1>
+            </div>
+            <div class="flex items-center justify-center text-black text-sm border-2 rounded-md border-black ">
+                <p class="p-2">
+                    متاسفانه به علت بروز مشکل در درگاه بانکی ، پرداخت شما ناموفق بوده است.
+                    در صورت عدم برگشت پول ظرف مدت <span class="font-bold text-rose-500">72 ساعت</span> با پشتیبانی درگاه بانک مربوطه تماس بگیرید.
+
+                </p>
+            </div>
+        </section>
+        @break
+    @case('container')
+        <section class="px-2">
+            <article
+                class=" grid grid-cols-2 border-2 border-black rounded-md text-black justify-between divide-x-2 divide-x-reverse divide-black">
+                <div class="text-right text-sm space-y-2 divide-y-2 divide-black bg-sky-500 text-white">
+                    <p class="pr-1">
+                        تاریخ و زمان خرید:
+                    </p>
+                    <p class="pr-1">
+                        شماره سفارش:
+                    </p>
+                    <p class="pr-1">
+                        درگاه پرداخت:
+                    </p>
+                </div>
+                <div class="text-left text-sm space-y-2 divide-y-2 divide-black ">
+                    <p class="pl-2">
+                        1403/06/09 15:17:49
+                    </p>
+                    <p class="pl-2">
+                        #6577214
+                    </p>
+                    <div class="pl-2 flex items-center justify-end space-x-1 space-x-reverse">
+                        <img src="{{asset('src/images/bankSamanSvg.svg')}}" alt="" class="w-6 h-6">
+                        <p>بانک سامان</p>
+                    </div>
+                </div>
+            </article>
+        </section>
+        <div class="min-w-full flex items-center justify-center mt-16 text-black">
+            <p>با تشکر از همراهی شما ، تیم ساینا ارز</p>
+            <img src="{{asset('src/images/icon.svg')}}" alt="">
+        </div>
+        @break
+@endswitch
--- resources/views/components/email-success.blade.php
+++ resources/views/components/email-success.blade.php
@@ -1,3 +1,59 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+@switch($type)
+    @case('message-box')
+        <section class="space-y-3">
+            <div class="flex justify-center min-w-full items-center">
+                <h1 class="font-bold text-lg text-green-500">خرید موفق کارت هدیه (ووچر)</h1>
+                <img src="{{asset('src/images/Group 414.png')}}" class="w-8 h-8 mt-3">
+            </div>
+            <div class="flex items-center justify-center text-black text-sm ">
+                <p>خرید کارت هدیه 5 دلاری و کسر مبلغ از کیف پول</p>
+            </div>
+        </section>
+        @break
+    @case('container')
+        <section class="px-2">
+            <article
+                class=" grid grid-cols-2 border-2 border-black rounded-md text-black justify-between divide-x-2 divide-x-reverse divide-black">
+                <div class="text-right text-sm space-y-2 divide-y-2 divide-black bg-sky-500 text-white">
+                    <p class="pr-1">
+                        تاریخ و زمان خرید:
+                    </p>
+                    <p class="pr-1">
+                        شماره ووچر:
+                    </p>
+                    <p class="pr-1">
+                        کدفعال سازی ووچر:
+                    </p>
+                    <p class="pr-1">
+                        شماره سفارش:
+                    </p>
+                    <p class="pr-1">
+                        مبلغ سفارش:
+                    </p>
+                </div>
+                <div class="text-left text-sm space-y-2 divide-y-2 divide-black ">
+                    <p class="pl-2">
+                        1403/06/09 15:17:49
+                    </p>
+                    <p class="pl-2">
+                        3862850767
+                    </p>
+                    <p class="pl-2">
+                        8683760825678912
+                    </p>
+                    <p class="pl-2">
+                        #6577214
+                    </p>
+                    <p class="pl-2">
+                        343 ریال
+                    </p>
+                </div>
+            </article>
+        </section>
+        <div class="min-w-full flex items-center justify-center mt-16 text-black">
+            <p>با تشکر از همراهی شما ، تیم ساینا ارز</p>
+            <img src="{{asset('src/images/icon.svg')}}" alt="">
+
+        </div>
+        @break
+@endswitch
