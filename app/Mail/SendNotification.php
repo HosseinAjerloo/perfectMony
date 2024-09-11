197dcb2ac8c6b4e82ce43b864dec246437e849a4
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-09-11 10:12:40 +0330

    test email

--- app/Mail/SendNotification.php
+++ app/Mail/SendNotification.php
@@ -39,7 +39,7 @@ class SendNotification extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'Email.index',
+            markdown: 'Email.index',
         );
     }
 
