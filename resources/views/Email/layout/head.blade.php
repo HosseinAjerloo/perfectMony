d74bb0268c7853a25d6c172957d389d4b7c2b67c
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-09-11 10:35:23 +0330

    test

--- resources/views/Email/layout/head.blade.php
+++ resources/views/Email/layout/head.blade.php
@@ -7,6 +7,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ورود به ساینا ارز</title>
-    <link rel="stylesheet" href="{{mix('resources/css/app.css')}}">
+    @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{asset('src/FontAwesome/css/all.css')}}">
 </head>
