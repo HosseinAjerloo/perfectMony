87c0f8fc8159dda167ff65a9d395b550972f9f74
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-10-14 14:35:28 +0330

    sync

--- resources/views/Auth/simpleLogin.blade.php
+++ resources/views/Auth/simpleLogin.blade.php
@@ -14,15 +14,22 @@
             </p>
         </div>
 
-        <form class="py-5 px-3  flex justify-center items-center flex-col  space-y-7" action="{{route('login.simple-post')}}" method="post">
+        <form class="py-5 px-3  flex justify-center items-center flex-col  space-y-7"
+              action="{{route('login.simple-post')}}" method="post">
             @csrf
             <input type="password" placeholder="کلمه عبور" name="password"
                    class="px-16 py-2 rounded-md ring-8 ring-gray-400 ring-opacity-35 placeholder:font-yekan outline-none w-full placeholder:text-center placeholder:text-gray-400 text-gray-900 text-center">
 
-            <div class="py-5 px-3  flex justify-center items-center space-x-2 space-x-reverse">
-                <a href="{{route('forgotPassword')}}" class="text-sky-500">فراموشی رمز عبور</a>
-                <button class="py-2 px-4 rounded-md font-semibold bg-sky-400  " type="submit">ورود به سایناارز</button>
-
+            <div class="py-5 px-3  flex justify-center items-center space-x-2 space-x-reverse flex-col">
+                <div class="py-5 px-3  flex justify-center items-center space-x-2 space-x-reverse">
+                    <button class="py-2 px-4 rounded-md font-semibold bg-sky-400  " type="submit">ورود به سایناارز
+                    </button>
+                    <a href="{{route('forgotPassword')}}" class=" py-2 px-4 rounded-md font-semibold bg-sky-400 ">فراموشی رمز عبور</a>
+                </div>
+                <div>
+                    <a href="{{route('login.login-BySms')}}" class="py-2 px-4 rounded-md font-semibold bg-sky-400 mt-2 inline-block">ورود
+                        با پیامک</a>
+                </div>
             </div>
 
         </form>
